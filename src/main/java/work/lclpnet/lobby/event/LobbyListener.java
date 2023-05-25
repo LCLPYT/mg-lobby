35f580467f38ab32d85d27eb0ca072be7101c299
LCLP <lukas@lclpnet.work>
2023-05-25 15:29:08 +0200

    teleport player back up to spawn again

--- src/main/java/work/lclpnet/lobby/event/LobbyListener.java
+++ src/main/java/work/lclpnet/lobby/event/LobbyListener.java
@@ -1,27 +1,35 @@
 package work.lclpnet.lobby.event;
 
-import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import work.lclpnet.kibu.hook.ServerPlayConnectionHooks;
 import work.lclpnet.kibu.hook.entity.ServerLivingEntityHooks;
 import work.lclpnet.kibu.hook.player.PlayerFoodHooks;
 import work.lclpnet.kibu.hook.player.PlayerInventoryHooks;
+import work.lclpnet.kibu.hook.player.PlayerMoveCallback;
+import work.lclpnet.kibu.hook.util.PositionRotation;
 import work.lclpnet.kibu.hook.world.BlockModificationHooks;
 import work.lclpnet.kibu.hook.world.WorldPhysicsHooks;
 import work.lclpnet.kibu.plugin.hook.HookListenerModule;
 import work.lclpnet.kibu.plugin.hook.HookRegistrar;
 import work.lclpnet.lobby.api.LobbyManager;
+
+import java.util.Set;
 
 public class LobbyListener implements HookListenerModule {
 
@@ -58,7 +66,21 @@ public class LobbyListener implements HookListenerModule {
         registrar.registerHook(WorldPhysicsHooks.CAULDRON_PRECIPITATION, (world, pos, newState) -> isLobby(world));
         registrar.registerHook(WorldPhysicsHooks.FROST_WALKER_FREEZE, (world, pos, entity) -> cancelLobbyAction(entity));
         registrar.registerHook(PlayerInventoryHooks.DROP_ITEM, (player, slot) -> cancelLobbyAction(player));
-        registrar.registerHook(ServerLivingEntityHooks.ALLOW_DAMAGE, (entity, source, amount) -> source.isOf(DamageTypes.OUT_OF_WORLD));
+        registrar.registerHook(ServerLivingEntityHooks.ALLOW_DAMAGE, this::allowDamage);
+        registrar.registerHook(PlayerMoveCallback.HOOK, this::onMove);
+    }
+
+    private boolean onMove(ServerPlayerEntity player, PositionRotation from, PositionRotation to) {
+        ServerWorld world = player.getWorld();
+
+        if (isLobby(world) && to.getY() < world.getBottomY()) {
+            // teleport player back to spawn location
+            Vec3d spawn = lobbyManager.getLobbySpawn();
+
+            player.teleport(world, spawn.getX(), spawn.getY(), spawn.getZ(), Set.of(), 0, 0);
+        }
+
+        return false;  // false = allow movement
     }
 
     private void onJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
@@ -97,5 +119,9 @@ public class LobbyListener implements HookListenerModule {
 
     private boolean onModify(World world, BlockPos pos, Entity entity) {
         return cancelLobbyAction(entity);
+    }
+
+    private boolean allowDamage(LivingEntity entity, DamageSource source, float amount) {
+        return isLobby(entity.getWorld()) && source.isOf(DamageTypes.OUT_OF_WORLD);
     }
 }
