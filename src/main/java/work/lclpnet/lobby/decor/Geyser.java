a26074477f8890b687992563390856f7764daf4d
tilln <tillnachtigall2@gmail.com>
2023-05-28 15:46:40 +0200

    added new geyser animation

--- src/main/java/work/lclpnet/lobby/decor/Geyser.java
+++ src/main/java/work/lclpnet/lobby/decor/Geyser.java
@@ -22,6 +22,8 @@ public class Geyser {
     private int eruptionTicks = -1;
     private Box box;
 
+    private int particleDelay;
+
     public Geyser(ServerWorld world, BlockPos position) {
         this.world = world;
         this.position = position;
@@ -55,12 +57,13 @@ public class Geyser {
 
     private void erupt() {
         eruptionTicks = 120 + random.nextInt(100);  // 6-11 seconds
+        particleDelay = 6;
         float eruptionStrength = 0.4f + random.nextFloat() * 0.6f;  // 0.4-1.0 strength
 
         Vec3d pos = position.toCenterPos();
         box = new Box(
                 pos.x - 2, pos.y, pos.z - 2,
-                pos.x + 2, pos.y + eruptionStrength * 20, pos.z + 2
+                pos.x + 2, pos.y + eruptionStrength * 35, pos.z + 2
         );
 
         double x = pos.getX();
@@ -77,9 +80,17 @@ public class Geyser {
         double y = pos.getY();
         double z = pos.getZ();
 
+        if (particleDelay == 0) {
+            world.spawnParticles(ParticleTypes.SNOWFLAKE, x, y + box.getYLength(), z, 40, 0.3, 0.5, 0.3, 0.15);
+            world.spawnParticles(ParticleTypes.DOLPHIN, x, y + box.getYLength(), z, 15, 1.5, 0.5, 1.5, 1);
+        } else {
+            particleDelay--;
+        }
+
         world.playSound(null, x, y, z, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.AMBIENT, 0.15f, 1 + (random.nextFloat() * 0.4f - 0.2f));
-        world.spawnParticles(ParticleTypes.CLOUD, x, y, z, 10, 0.25, box.getYLength(), 0.25, 0.15);
-        world.spawnParticles(ParticleTypes.CLOUD, x, y, z, 5, 1, 0.1, 1, 0.2);
+        world.spawnParticles(ParticleTypes.POOF, x, y, z, 15, 0.25, box.getYLength() / 2, 0.25, 0.1);
+        world.spawnParticles(ParticleTypes.DOLPHIN, x, y, z, 8, 0.3, box.getYLength() / 2, 0.3, 0.3);
+        world.spawnParticles(ParticleTypes.SPIT, x, y, z, 10, 1.5, 0.1, 1.5, 0.15);
 
         for (Entity entity : findCollidingEntities()) {
             entity.velocityDirty = true;  // TODO not working
