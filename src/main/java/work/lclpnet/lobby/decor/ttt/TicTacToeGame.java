26c366a5d25d9ad710825d0974ce47c8a1518202
LCLP <lukas@lclpnet.work>
2023-06-21 16:01:41 +0200

    minor style adjustments to TicTacToeGame

--- src/main/java/work/lclpnet/lobby/decor/ttt/TicTacToeGame.java
+++ src/main/java/work/lclpnet/lobby/decor/ttt/TicTacToeGame.java
@@ -2,76 +2,83 @@ package work.lclpnet.lobby.decor.ttt;
 
 public class TicTacToeGame implements TicTacToe {
 
-    int[][] gameArr = new int[3][3];
-    int winner = -1;
+    private final int[][] gameArr = new int[3][3];
+    private int winner = -1;
 
     @Override
     public boolean doMove(int player, int x, int y) {
-        if (x < 0 || x >= gameArr.length || y < 0 || y >= gameArr.length)
-            return false;
-        if (gameArr[x][y] != 0) {
-            return false;
-        }
+        if (x < 0 || x >= gameArr.length || y < 0 || y >= gameArr.length || gameArr[x][y] != 0) return false;
+
         gameArr[x][y] = player;
+
         return true;
     }
 
     @Override
     public boolean isGameOver() {
         for (int i = 0; i < 3; i++) {
-
-            //check rows for streak
+            // check rows for streak
             boolean streak = true;
+
             for (int j = 0; j < 3; j++) {
                 if (gameArr[i][j] != gameArr[i][0] || gameArr[i][0] == 0) {
                     streak = false;
                     break;
                 }
             }
+
             if (streak) {
                 winner = gameArr[i][0];
                 return true;
             }
 
-            //check columns for streak
+            // check columns for streak
             streak = true;
+
             for (int k = 0; k < 3; k++) {
                 if (gameArr[k][i] != gameArr[0][i] || gameArr[0][i] == 0) {
                     streak = false;
                     break;
                 }
             }
+
             if (streak) {
                 winner = gameArr[0][i];
                 return true;
             }
 
-            //check left to right diagonal for streak:
+            // check left to right diagonal for streak
             streak = true;
+
             for (int j = 0; j < 3; j++) {
                 if (gameArr[j][j] != gameArr[0][0] || gameArr[j][j] == 0) {
                     streak = false;
                     break;
                 }
             }
+
             if (streak) {
                 winner = gameArr[0][0];
                 return true;
             }
 
-            //check right to left diagonal for streak:
+            // check right to left diagonal for streak
             streak = true;
+
             for (int k = 0; k < 3; k++) {
                 if (gameArr[k][2 - k] != gameArr[0][2] || gameArr[k][2 - k] == 0) {
                     streak = false;
                     break;
                 }
             }
+
             if (streak) {
                 winner = gameArr[0][2];
                 return true;
             }
         }
+
+        // check draw
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (gameArr[i][j] == 0) {
@@ -79,7 +86,9 @@ public class TicTacToeGame implements TicTacToe {
                 }
             }
         }
+
         winner = 0;
+
         return true;
     }
 
