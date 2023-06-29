192642a8b844923ca8625f81233a4b95849e70ce
LCLP <lukas@lclpnet.work>
2023-06-29 15:45:55 +0200

    migrate to translations4j 2.3.0

--- src/main/java/work/lclpnet/lobby/service/LobbyTranslationProvider.java
+++ src/main/java/work/lclpnet/lobby/service/LobbyTranslationProvider.java
@@ -3,19 +3,20 @@ package work.lclpnet.lobby.service;
 import org.slf4j.Logger;
 import work.lclpnet.lobby.LobbyAPI;
 import work.lclpnet.translations.loader.TranslationProvider;
-import work.lclpnet.translations.loader.language.ClassLoaderLanguageLoader;
 import work.lclpnet.translations.loader.language.LanguageLoader;
+import work.lclpnet.translations.loader.language.UrlLanguageLoader;
 
+import java.net.URL;
 import java.util.List;
 
 public class LobbyTranslationProvider implements TranslationProvider {
 
     @Override
     public LanguageLoader create() {
-        ClassLoader classLoader = getClass().getClassLoader();
+        URL[] urls = UrlLanguageLoader.getResourceLocations(this);
         List<String> resourceDirectories = List.of("lang/");
         Logger logger = LobbyAPI.getInstance().getManager().getLogger();
 
-        return new ClassLoaderLanguageLoader(classLoader, resourceDirectories, logger);
+        return new UrlLanguageLoader(urls, resourceDirectories, logger);
     }
 }
