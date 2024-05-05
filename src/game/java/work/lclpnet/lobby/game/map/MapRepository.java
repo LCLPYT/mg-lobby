76aa2192907487da52eaa17f6a29c73f509c9c08
LCLP <lukas@lclpnet.work>
2024-05-05 14:42:17 +0200

    fix map repository source uri resolving for Java 21

--- src/game/java/work/lclpnet/lobby/game/map/MapRepository.java
+++ src/game/java/work/lclpnet/lobby/game/map/MapRepository.java
@@ -1,7 +1,10 @@
 package work.lclpnet.lobby.game.map;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -18,14 +21,30 @@ public interface MapRepository {
             return Optional.empty();
         }
 
+        URI uri;
+
         try {
-            URI uri = URI.create(source);
+            uri = URI.create(source);
+        } catch (Exception e) {
+            // invalid URI
+            return Optional.empty();
+        }
 
-            return Optional.of(uri);
-        } catch (IllegalArgumentException ignored) {}
+        // check if source URI is a valid URL
+        try {
+            URL url = uri.toURL();
 
-        var uri = info.uri().resolve(source.replace('\\', '/'));
+            // source is a valid URL, convert it back to URI
+            return Optional.of(url.toURI());
+        } catch (URISyntaxException ignored) {
+            // invalid URI
+            return Optional.empty();
+        } catch (MalformedURLException | IllegalArgumentException ignored) {
+            // source isn't a valid URL, use relative path behaviour instead...
+        }
 
-        return Optional.of(uri);
+        var relativeUri = info.uri().resolve(source.replace('\\', '/'));
+
+        return Optional.of(relativeUri);
     }
 }
