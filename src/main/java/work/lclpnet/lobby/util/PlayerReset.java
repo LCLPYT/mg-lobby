ca65c23694547560556b511374ddc7e57f7910b3
LCLP <lukas@lclpnet.work>
2024-08-08 14:53:23 +0200

    reset more attributes and stuck arrow count on joining lobby

--- src/main/java/work/lclpnet/lobby/util/PlayerReset.java
+++ src/main/java/work/lclpnet/lobby/util/PlayerReset.java
@@ -2,7 +2,6 @@ package work.lclpnet.lobby.util;
 
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -13,6 +12,8 @@ import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 import work.lclpnet.kibu.access.VelocityModifier;
 import work.lclpnet.kibu.hook.util.PlayerUtils;
+
+import static net.minecraft.entity.attribute.EntityAttributes.*;
 
 public class PlayerReset {
 
@@ -29,6 +30,7 @@ public class PlayerReset {
         player.setExperienceLevel(0);
         player.setExperiencePoints(0);
         player.setFireTicks(0);
+        player.setStuckArrowCount(0);
         player.setOnFire(false);
         VelocityModifier.setVelocity(player, Vec3d.ZERO);
 
@@ -58,31 +60,38 @@ public class PlayerReset {
     }
 
     public static void resetAttributes(ServerPlayerEntity player) {
-        resetAttribute(player, EntityAttributes.GENERIC_ARMOR);
-        resetAttribute(player, EntityAttributes.GENERIC_ARMOR_TOUGHNESS);
-        resetAttribute(player, EntityAttributes.GENERIC_ATTACK_DAMAGE);
-        resetAttribute(player, EntityAttributes.GENERIC_ATTACK_KNOCKBACK);
-        resetAttribute(player, EntityAttributes.GENERIC_ATTACK_SPEED);
-        resetAttribute(player, EntityAttributes.GENERIC_FLYING_SPEED);
-        resetAttribute(player, EntityAttributes.GENERIC_FOLLOW_RANGE);
-        resetAttribute(player, EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE);
-        resetAttribute(player, EntityAttributes.GENERIC_LUCK);
-        resetAttribute(player, EntityAttributes.GENERIC_MAX_ABSORPTION);
-        resetAttribute(player, EntityAttributes.GENERIC_MAX_HEALTH);
-        resetAttribute(player, EntityAttributes.GENERIC_MOVEMENT_SPEED);
-        resetAttribute(player, EntityAttributes.GENERIC_SCALE);
-        resetAttribute(player, EntityAttributes.GENERIC_STEP_HEIGHT);
-        resetAttribute(player, EntityAttributes.GENERIC_JUMP_STRENGTH);
-        resetAttribute(player, EntityAttributes.PLAYER_BLOCK_INTERACTION_RANGE);
-        resetAttribute(player, EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE);
-        resetAttribute(player, EntityAttributes.PLAYER_BLOCK_BREAK_SPEED);
-        resetAttribute(player, EntityAttributes.GENERIC_GRAVITY);
-        resetAttribute(player, EntityAttributes.GENERIC_SAFE_FALL_DISTANCE);
-        resetAttribute(player, EntityAttributes.GENERIC_FALL_DAMAGE_MULTIPLIER);
+        resetAttribute(player, GENERIC_ARMOR);
+        resetAttribute(player, GENERIC_ARMOR_TOUGHNESS);
+        resetAttribute(player, GENERIC_ATTACK_DAMAGE);
+        resetAttribute(player, GENERIC_ATTACK_KNOCKBACK);
+        resetAttribute(player, GENERIC_ATTACK_SPEED);
+        resetAttribute(player, PLAYER_BLOCK_BREAK_SPEED);
+        resetAttribute(player, PLAYER_BLOCK_INTERACTION_RANGE);
+        resetAttribute(player, GENERIC_BURNING_TIME);
+        resetAttribute(player, GENERIC_EXPLOSION_KNOCKBACK_RESISTANCE);
+        resetAttribute(player, PLAYER_ENTITY_INTERACTION_RANGE);
+        resetAttribute(player, GENERIC_FALL_DAMAGE_MULTIPLIER);
+        resetAttribute(player, GENERIC_GRAVITY);
+        resetAttribute(player, GENERIC_JUMP_STRENGTH);
+        resetAttribute(player, GENERIC_KNOCKBACK_RESISTANCE);
+        resetAttribute(player, GENERIC_LUCK);
+        resetAttribute(player, GENERIC_MAX_ABSORPTION);
+        resetAttribute(player, GENERIC_MAX_HEALTH);
+        resetAttribute(player, PLAYER_MINING_EFFICIENCY);
+        resetAttribute(player, GENERIC_MOVEMENT_EFFICIENCY);
+        resetAttribute(player, GENERIC_MOVEMENT_SPEED);
+        resetAttribute(player, GENERIC_OXYGEN_BONUS);
+        resetAttribute(player, GENERIC_SAFE_FALL_DISTANCE);
+        resetAttribute(player, GENERIC_SCALE);
+        resetAttribute(player, PLAYER_SNEAKING_SPEED);
+        resetAttribute(player, GENERIC_STEP_HEIGHT);
+        resetAttribute(player, PLAYER_SUBMERGED_MINING_SPEED);
+        resetAttribute(player, PLAYER_SWEEPING_DAMAGE_RATIO);
+        resetAttribute(player, GENERIC_WATER_MOVEMENT_EFFICIENCY);
     }
 
     public static void resetAttribute(ServerPlayerEntity player, RegistryEntry<EntityAttribute> attribute) {
-        if (attribute == EntityAttributes.GENERIC_MOVEMENT_SPEED) {
+        if (attribute == GENERIC_MOVEMENT_SPEED) {
             setAttribute(player, attribute, player.getAbilities().getWalkSpeed());
             return;
         }
@@ -105,7 +114,7 @@ public class PlayerReset {
     public static void modifyWalkSpeed(ServerPlayerEntity player, float value, boolean update) {
         player.getAbilities().setWalkSpeed(value);
 
-        EntityAttributeInstance attribute = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        EntityAttributeInstance attribute = player.getAttributeInstance(GENERIC_MOVEMENT_SPEED);
 
         if (attribute != null) {
             attribute.setBaseValue(value);
